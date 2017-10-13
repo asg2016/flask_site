2741fea884ef9355c3572fc07cccf5a87e2c8916
asg2016 <asg0182@gmail.com>
2017-10-13 16:10:24 +0500

    add example web pages

---
+++ market/schema.sql
@@ -0,0 +1,20 @@
+drop table if exists ;
+create table goods (
+  id integer primary key autoincrement,
+  title text not null,
+  description text null,
+  cpu text not null,
+  cpu_freq text not null,
+  ram text not null,
+  hdd text not null,
+  display text not null,
+  videocard text not null,
+  weight real not null,
+  dvd integer not null,
+  lte integer not null,
+  blutooth integer not null,
+  wifi integer not null,
+  colors text not null,
+  images text not null,
+  price real no null
+);
